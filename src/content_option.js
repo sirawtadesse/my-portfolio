67ddbba2e10304477a1a48d3433d8831045a4edd
sirawtadesse <sershotadesse@gmail.com>
2023-11-05 14:41:48 +0300

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,124 @@
+const logotext = "SIRAW";
+const meta = {
+    title: "Siraw Tadesse",
+    description: "I’m Siraw Tadesse and i am 4th year information system student at AAU",
+};
+
+const introdata = {
+    title: "I’m Siraw Tadesse",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I am interested in networking",
+    },
+    description: "Throughout my studies, i have probably been exposed to various topics such as database management, programming, systems analysis and design, network administration, cybersecurity, and project management. These areas of study are essential for understanding how to design, develop, implement, and manage information systems within organizations.",
+    your_img_url: "img/s1.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I’m Siraw Tadesse and 4th year information system student at AAU.",
+};
+const worktimeline = [{
+        jobtitle: "project for class",
+        where: "Addis Ababa University",
+        date: "2023",
+    },
+    {
+        jobtitle: "Project for class",
+        where: "Addis Ababa University",
+        date: "2023",
+    },
+    {
+        jobtitle: "Class project",
+        where: "Addis Ababa University",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "UI refers to the screens, buttons, toggles, icons, and other visual elements that you interact with when using a website, app, or electronic device. UX refers to the entire interaction you have with a product, including how you feel about the interaction.",
+    },
+    {
+        title: "networking",
+        description: "Networking is the process of making connections and building relationships. These connections can provide you with advice and contacts, which can help you make informed career decisions.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "WordPress is an open-source content management system (CMS) used to build websites. A 2022 study by W3 Techs indicates that 43 percent of all websites on the internet use WordPress.",
+    },
+];
+
+const dataportfolio = [{
+        img: "img/c1.png",
+        description: "Product management System.",
+        link: "https://https://github.com/sirawtadesse/django_product_management",
+    },
+    
+    
+    
+    {
+        img: "img/c2.png",
+        description: "My Personal Website In HTML, CSS, JS.",
+        link: "https://github.com/sirawtadesse/mypersonalwebsite",
+    },
+   
+    
+   
+    {
+        img: "img/blog.jpg",
+        description: "Blog In Nextjs-Frontend.",
+        link: "https://github.com/sirawtadesse/Sblog-Nextjs",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sirawbizutadesse21@gmail.com",
+    YOUR_FONE: "(+251)919901362",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
